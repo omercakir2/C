eafa246d62a0ae6e56442f4c1abd8bfb7d07ec4c
omercakir2 <omercakir2@DESKTOP-E5QDEKC.>
2024-11-03 11:04:55 +0300

    Solved the lab exam question

--- Project1/Project1/Code1.cpp
+++ Project1/Project1/Code1.cpp
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
+
 int main(void) {
 	srand(time(NULL));
-	int num = rand() % 1100 + 1;
-	printf("The number generatered is  : %d", num);
+	//int num = rand() % 1100 + 1;
+	int num = 153;
+	printf("The number generatered is  : %d \n", num);
 
+	int res = 0;
 	for (int i = 10; i <= num*10; i *= 10) {
-		int digit = num % i / (i / 10);
-		printf("The digit is : %d", digit);
+
+		int digit = num % i / (i / 10);//printf("The digit is : %d\n", digit);
+		res += pow(digit, 3);//printf("The res is : %d\n", res);	
+	}
+	if (res == num) {
+		printf(" %d is a narcistic number \n",num);
+	}
+	else {
+		printf(" %d is NOT a narcistic number \n", num);
 	}
 
 	return 0;
