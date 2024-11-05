2c311654c3dfd06bf898abc5687f5f0dae660c59
omercakir2 <omercakir2@DESKTOP-E5QDEKC.>
2024-11-05 13:16:34 +0300

    add some features like delete and update

---
+++ Hashing_Sols/HashingV1/hashingv1.cpp
@@ -0,0 +1,30 @@
+//First version of the code
+#include <stdio.h>
+#include <string.h>
+#define MAX_NAME 256
+#define TABLE_SIZE 10
+
+typedef struct {
+	char name[MAX_NAME];
+	int age;
+	//we can more things later
+}person;
+
+unsigned int hash(const char* name) {
+	int lenght = strlen(name);
+	unsigned int hash_value = 0;
+	for (int i = 0; i < lenght;i++) {
+		hash_value += name[i];
+
+	}
+	hash_value = hash_value % 10;
+	return hash_value;
+}
+
+int main(void) {
+	printf("Omer ==> %u \n", hash("Omer"));// we use %u because it's for unsigned integer
+	printf("Faruk ==> %u \n", hash("Faruk"));
+	printf("Emre ==> %u \n", hash("Emre"));
+	printf("Gulcan ==> %u \n", hash("Gulcan"));
+	return 0;
+}
